ee3281d46c4843488fe154c234fe81a0308108e7
Jeff Lee <leejeffc@gmail.com>
2014-02-27 23:43:52 -0800

    Fix Angular app instantiation

--- scripts/app.js
+++ scripts/app.js
@@ -1,12 +1,8 @@
 (function(){
   'use strict';
 
-  angular.module('chatUIApp')
-    .config(function($routeProvider){
-      // handle routes here
-      // $routeProvider.when('/path', {
-      //   templateURL: '../views/view.html',
-      //   controller: 'controllers/Ctrl.js'
-      // }
+  angular.module('chatUIApp', [])
+    .config(function(){
+      // handle routes here using $routeProvider
     });
 })();
