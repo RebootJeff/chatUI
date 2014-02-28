f4e475a0f90825addaa0ff9422166740f7edee33
Jeff Lee <leejeffc@gmail.com>
2014-02-27 23:44:39 -0800

    Add basic ChatCtrl functionality

--- scripts/controllers/ChatCtrl.js
+++ scripts/controllers/ChatCtrl.js
@@ -1,4 +1,21 @@
-angular.module('chatUIApp').controller('ChatCtrl', function($scope) {
-  'use strict';
-  
-});
+angular.module('chatUIApp')
+  .controller('ChatCtrl', function($scope, FakeMsgData) {
+    'use strict';
+
+    $scope.sendMsg = function(){
+      var currentMoment = moment();
+      var newMsg = {
+        author: {
+          username: 'demo user',
+          avatar: 'default-avatar.jpg'
+        },
+        date: currentMoment.format('l'), // Format will be: m/d/yyyy
+        time: currentMoment.format('H:mm'), // Format will be military time
+        text: $scope.newMsgText
+      };
+
+      FakeMsgData.addMsg(newMsg);
+    };
+
+    setTimeout(FakeMsgData.getData, 1000);
+  });
