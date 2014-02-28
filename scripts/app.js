9d28f1faaf58c9e0d79d893fe56d0d525d9def09
Jeff Lee <leejeffc@gmail.com>
2014-02-27 18:01:50 -0800

    Add initial Angular app

---
+++ scripts/app.js
@@ -0,0 +1,12 @@
+(function(){
+  'use strict';
+
+  angular.module('chatUIApp')
+    .config(function($routeProvider){
+      // handle routes here
+      // $routeProvider.when('/path', {
+      //   templateURL: '../views/view.html',
+      //   controller: 'controllers/Ctrl.js'
+      // }
+    });
+})();
